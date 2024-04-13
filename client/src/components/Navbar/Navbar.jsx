4fa707a13b20d4b08672f40c944ce39f3e194c1a
Sarthak Patel <sarthakpatel230204@gmail.com>
2024-04-13 17:21:29 +0530

    Added react router and some routes

--- client/src/components/Navbar/Navbar.jsx
+++ client/src/components/Navbar/Navbar.jsx
@@ -1,9 +1,13 @@
 import React from 'react'
+import { Outlet } from 'react-router-dom'
 import styles from './Navbar.module.scss'
 
 const Navbar = () => {
   return (
+    <>
     <div>Navbar</div>
+    <Outlet />
+    </>
   )
 }
 
