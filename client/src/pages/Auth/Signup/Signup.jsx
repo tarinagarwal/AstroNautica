c0a3eb952c7d905127680b160a98e3011dbecb8a
VarunGBhat <141301727+VarunGBhat@users.noreply.github.com>
2024-04-14 00:30:07 +0530

    Adjusted the css of auth pages (#2)
    * Added some assests
    
    * Adjusted the css

--- client/src/pages/Auth/Signup/Signup.jsx
+++ client/src/pages/Auth/Signup/Signup.jsx
@@ -1,7 +1,6 @@
-import React, { useEffect, useState } from 'react'
+import React, { useEffect, useState } from "react";
 
 const Signup = () => {
-
   const [currentIndex, setCurrentIndex] = useState(0);
 
   const imagesWithText = [
@@ -14,7 +13,7 @@ const Signup = () => {
 
   useEffect(() => {
     const interval = setInterval(() => {
-      setCurrentIndex(prevIndex =>
+      setCurrentIndex((prevIndex) =>
         prevIndex === imagesWithText.length - 1 ? 0 : prevIndex + 1
       );
     }, 3000);
@@ -24,58 +23,63 @@ const Signup = () => {
 
   return (
     <>
+      <div
+        class="background"
+        style={{ backgroundImage: `url(${imagesWithText[currentIndex].url})` }}
+      >
+        <p class="backgroundText" className="background-text">
+          {imagesWithText[currentIndex].text}
+        </p>
+        <div class="signup-wrapper">
+          <form action="">
+            <h1>Register</h1>
+            <div class="input-box">
+              <input type="text" placeholder="First name" required />
+            </div>
+            <div class="input-box">
+              <input type="text" placeholder="Last name" required />
+            </div>
+            <div class="input-box">
+              <input type="text" placeholder="Username" required />
+              <i class="bx bxs-user"></i>
+            </div>
+            <div class="input-box">
+              <input
+                type="password"
+                placeholder="Password"
+                id="password"
+                required
+              />
+              <i class="bx bxs-hide" id="eyeicon" onclick="showPassword()"></i>
+            </div>
+            <div class="input-box">
+              <input
+                type="password"
+                placeholder="Confirm password"
+                id="confirm-password"
+                required
+              />
+              <i
+                class="bx bxs-hide"
+                id="eyeicon-confirm"
+                onclick="showPasswordagain()"
+              ></i>
+            </div>
 
-<div class="background" style={{backgroundImage: `url(${imagesWithText[currentIndex].url})`}}>
-      <p class="backgroundText" className="background-text" >
-      {imagesWithText[currentIndex].text}
-      </p>
-      <div class="wrapper">
-        <form action="">
-          <h1>Register</h1>
-          <div class="input-box">
-            <input type="text" placeholder="First name" required />
-          </div>
-          <div class="input-box">
-            <input type="text" placeholder="Last name" required />
-          </div>
-          <div class="input-box">
-            <input type="text" placeholder="Username" required />
-            <i class="bx bxs-user"></i>
-          </div>
-          <div class="input-box">
-            <input
-              type="password"
-              placeholder="Create password"
-              id="password"
-              required
-            />
-            <i class="bx bxs-hide" id="eyeicon" onclick="showPassword()"></i>
-          </div>
-          <div class="input-box">
-            <input
-              type="password"
-              placeholder="Conform password"
-              id="confirm-password"
-              required
-            />
-            <i
-              class="bx bxs-hide"
-              id="eyeicon-confirm"
-              onclick="showPasswordagain()"
-            ></i>
-          </div>
+            <button type="submit" class="btn">
+              Create
+            </button>
 
-          <button type="submit" class="btn">Create</button>
-
-          <div class="register-link">
-            <p>Already have an account? <a href="index.html">Login</a></p>
-          </div>
-        </form>
+            <div class="register-link">
+              <p>
+                Already have an account? <a href="index.html">Login</a>
+              </p>
+            </div>
+          </form>
+        </div>
       </div>
-    </div>
-    
     </>
-  )
-}
+  );
+};
 
-export default Signup
+export default Signup;
