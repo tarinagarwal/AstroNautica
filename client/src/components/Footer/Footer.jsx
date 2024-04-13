1d0dc3b775d247c6c11721ac6cacae4c542ee8e3
Sarthak Patel <sarthakpatel230204@gmail.com>
2024-04-14 00:18:03 +0530

    Added Footer

--- client/src/components/Footer/Footer.jsx
+++ client/src/components/Footer/Footer.jsx
@@ -1,9 +1,22 @@
 import React from 'react'
 import styles from './Footer.module.scss'
+import logo from '../../assests/logo.png'
+import { Link } from 'react-router-dom'
 
 const Footer = () => {
   return (
-    <div>Footer</div>
+    <>
+     <footer class = {styles.footer}>
+      <div class = {styles.footer_container}>
+        
+
+      
+      </div>
+     <div className = {styles.footer_bottom}>
+          <p>Copyright © 2024 AstroNautics - All Rights Reserved</p>
+     </div>
+     </footer>
+    </>
   )
 }
 
