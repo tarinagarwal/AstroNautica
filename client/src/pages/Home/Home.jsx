75a256fa3cf208973bd02a3cf0d3053f1a0ef9e3
VarunGBhat <141301727+VarunGBhat@users.noreply.github.com>
2024-04-14 01:44:41 +0530

    slightly designed the home page (#3)
    * Added some assests
    
    * Adjusted the css
    
    * adjusted some css of home some

--- client/src/pages/Home/Home.jsx
+++ client/src/pages/Home/Home.jsx
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from "react";
-import bgVideo from '../../assests/bgVideo.mp4'
-import './Home.scss'
+import bgVideo from "../../assests/bgVideo.mp4";
+import "./Home.scss";
 import axios from "axios";
 import API_BASE_URL from "../../config/config";
 
@@ -10,91 +10,88 @@ import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
 
 const Home = () => {
+  const [apodData, setApodData] = useState({});
+  const [apodImages, setApodImages] = useState([]);
 
-    const [apodData, setApodData] = useState({});
-    const [apodImages, setApodImages] = useState([]);
+  const settings = {
+    className: "center",
+    centerMode: true,
+    infinite: true,
+    centerPadding: "60px",
+    slidesToShow: 3,
+    autoplay: true,
+    autoplaySpeed: 2000,
+    speed: 300,
+    pauseOnHover: false,
+  };
 
-
-    const settings = {
-        className: "center",
-        centerMode: true,
-        infinite: true,
-        centerPadding: "60px",
-        slidesToShow: 3,
-        autoplay: true,
-        autoplaySpeed: 2000,
-        speed: 300,
-        pauseOnHover: false
-      };
-
-   useEffect(()=>{
-    const fetchApodData =  async() => {
-        const apod = await axios.post(`${API_BASE_URL}/v1/data/img/apod`);
-        console.log(apod.data)
-        setApodData(apod.data)
-    }
+  useEffect(() => {
+    const fetchApodData = async () => {
+      const apod = await axios.post(`${API_BASE_URL}/v1/data/img/apod`);
+      console.log(apod.data);
+      setApodData(apod.data);
+    };
 
     fetchApodData();
-   
-    const fetchApodImages =  async() => {
-        const apod = await axios.post(`${API_BASE_URL}/v1/data/img/apod`,{
-            count:10
-        });
-        console.log(apod.data)
-        setApodImages(apod.data)
-       }
-    
-       fetchApodImages();
-    
-   },[])
 
- 
+    const fetchApodImages = async () => {
+      const apod = await axios.post(`${API_BASE_URL}/v1/data/img/apod`, {
+        count: 10,
+      });
+      console.log(apod.data);
+      setApodImages(apod.data);
+    };
+
+    fetchApodImages();
+  }, []);
+
   return (
     <>
+      <div className="hero_section">
+        <video id="background-video" autoPlay loop muted>
+          <source src={bgVideo} type="video/mp4" />
+        </video>
+        <div class="searchbar">
+          <input type="text" name="" id="search" placeholder="Search here" />
+        </div>
+        <h1 class="big-text">Astrophotographer</h1>
+        <p class="small-text">
+          Embark on a cosmic journey through captivating images. Our
+          astrophotography showcases the beauty of the universe, capturing
+          celestial wonders in breathtaking detail. Explore distant galaxies,
+          nebulae, and star clusters through the lens of our passionate
+          photographers. Let the cosmos inspire and awe you.
+        </p>
+      </div>
+      <h1 className="heading">Some Astronomy Picture </h1>
 
-    <div className="hero_section">
-    <video
-        id="background-video"
-        autoPlay loop muted 
-      >
-        <source src={bgVideo} type="video/mp4" />
-      </video>
-      h1
-    </div>
-
-    <h1 className="heading">Some Astronomy Picture </h1>
-
-    <div className="picture_slider">
-
-    <Slider {...settings}>
+      <div className="picture_slider">
+        <Slider {...settings}>
           {apodImages.map((image, index) => (
-
             <div key={index} className="image">
               <img src={image.hdurl} alt="slideshow" />
             </div>
           ))}
-    </Slider>
+        </Slider>
+      </div>
 
-    </div>
+      <h1 className="heading">Astronomy Picture of Day</h1>
 
-
-
-    <h1 className="heading">Astronomy Picture of Day</h1>
-
-    <div className="apod_Container">
+      <div className="apod_Container">
         <img src={apodData.hdurl} alt="apod" />
         <div className="apod_info">
-            <h2>{apodData.title}</h2>
-            <p>{apodData.explanation}</p>
-            <div className="author_info">
-            <span> <FaRegCalendarAlt size={20}/><p>{apodData.date}</p> </span>
+          <h2>{apodData.title}</h2>
+          <p>{apodData.explanation}</p>
+          <div className="author_info">
+            <span>
+              {" "}
+              <FaRegCalendarAlt size={20} />
+              <p>{apodData.date}</p>{" "}
+            </span>
             <p>{apodData.copyright}</p>
-
-            </div>
-           
+          </div>
         </div>
-    </div>
-      
+      </div>
     </>
   );
 };
